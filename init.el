b9f1dcdf37521fd674164611a089b260ef144755
Peter Manahan <prmanahan@gmail.com>
2015-08-31 20:47:50 -0300

    added Menlo 18 font

--- init.el
+++ init.el
@@ -1,80 +1,17 @@
-(setenv "PATH" (shell-command-to-string "echo $PATH"))
-
-
 (require 'package)
-;
-;
-;; (add-to-list 'package-archives
-;; 	     '("marmalade" . "http://marmalade-repo.org/packages/")
-;;              )
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-;; user custom config
-(progn 
-(setq cust-user-config (concat user-emacs-directory user-login-name ".el")
-      cust-user-dir (concat user-emacs-directory user-login-name))
-
-(add-to-list 'load-path cust-user-dir)
-(when (file-exists-p cust-user-config ) (load cust-user-config))
-(when (file-exists-p cust-user-dir)
-  (mapc 'load (directory-files cust-user-dir nil "^[^#].*el$"))))
-
-(require 'nrepl)
- 
-;; Configure nrepl.el
-(setq nrepl-hide-special-buffers t)
-(setq nrepl-popup-stacktraces-in-repl t)
-(setq nrepl-history-file "~/.emacs.d/nrepl-history")
- 
-;; Some default eldoc facilities
-(add-hook 'nrepl-connected-hook
-(defun pnh-clojure-mode-eldoc-hook ()
-(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
-(nrepl-enable-on-existing-clojure-buffers)))
- 
-;; Repl mode hook
-(add-hook 'nrepl-mode-hook 'subword-mode)
- 
-;; Auto completion for NREPL
-(require 'ac-nrepl)
-(eval-after-load "auto-complete"
-'(add-to-list 'ac-modes 'nrepl-mode))
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-
-(load-file "/Users/manahan/hacking/src/javert/nrepl-inspect.el")
-(define-key nrepl-mode-map (kbd "C-c i") 'nrepl-inspect)
-
-(require 'nrepl-ritz) ;; after (require 'nrepl)
- 
-;; Ritz middleware
-(define-key nrepl-interaction-mode-map (kbd "C-c C-j") 'nrepl-javadoc)
-(define-key nrepl-mode-map (kbd "C-c C-j") 'nrepl-javadoc)
-(define-key nrepl-interaction-mode-map (kbd "C-c C-a") 'nrepl-apropos)
-(define-key nrepl-mode-map (kbd "C-c C-a") 'nrepl-apropos)
-
-;; fix the PATH variable
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
-
-(when window-system (set-exec-path-from-shell-PATH))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(menu-bar-mode t))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#fcf4dc" :foreground "#52676f" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "apple" :family "Monaco")))))
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Menlo")))))
